337b708705987342f9160b94604b28df9beb622f
msk29 <msk29@DESKTOP-BHA25FJ>
2024-03-04 13:32:53 +0530

    Add project files.

---
+++ ConsoleApp4/Planner/MathPlanner.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+public class MathPlanner
+{
+    [KernelFunction]
+    [Description("Returns back the Math calculation.")]
+    [return: Description("Return the calculated answer")]
+    public async Task<string> GenerateRequiredStepsAsync(
+        Kernel kernel,
+        [Description("Number 1")] string number1,
+        [Description("Number 2")] string number2
+    )
+    {
+        // Prompt the LLM to generate a list of steps to complete the task
+        var result = await kernel.InvokePromptAsync($"""
+        I'm going to calculate the {number1} and {number2} on behalf of user.
+        """, new() {
+            { "Number1", number1 },
+            { "Number2", number2 }
+        });
+
+        // Return the plan back to the agent
+        return result.ToString();
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using Plugins;
+
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion("chatdj", "https://azoaidj.openai.azure.com/", "7f629b03e6524736a64127d31abf5029");
+builder.Plugins.AddFromType<MathPlugin>();
+builder.Plugins.AddFromType<MathPlanner>();
+Kernel kernel = builder.Build();
+
+ChatHistory history = new(); 
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+Console.Write("User > ");
+string? userInput;
+while ((userInput = Console.ReadLine()) != null)
+{
+    history.AddUserMessage(userInput);
+
+    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+    {
+        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+    };
+
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
+                        history,
+                        executionSettings: openAIPromptExecutionSettings,
+                        kernel: kernel);
+
+    string fullMessage = "";
+    var first = true;
+    await foreach (var content in result)
+    {
+        if (content.Role.HasValue && first)
+        {
+            Console.Write("Assistant > ");
+            first = false;
+        }
+        Console.Write(content.Content);
+        fullMessage += content.Content;
+    }
+    Console.WriteLine();
+
+    history.AddAssistantMessage(fullMessage);
+
+    Console.Write("User > ");
+}
